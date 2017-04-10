d51f94e7349b071bc06272f45745c5419dafd853
Carl Chen <ccf.developer@gmail.com>
2017-04-10 09:59:00 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# 依赖 
+
+- Security.framework
+- CoreTelephony.framework
+- AdSupport.framework
+- SystemConfiguration.framework
+- libz.tbd
---
+++ TalkingDataSDK/TalkingDataSDK.h
@@ -0,0 +1,19 @@
+//
+//  TalkingDataSDK.h
+//  TalkingDataSDK
+//
+//  Created by Carl on 10/4/2017.
+//  Copyright © 2017 ricebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TalkingDataSDK.
+FOUNDATION_EXPORT double TalkingDataSDKVersionNumber;
+
+//! Project version string for TalkingDataSDK.
+FOUNDATION_EXPORT const unsigned char TalkingDataSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TalkingDataSDK/PublicHeader.h>
+
+#import "TalkingDataAppCpa.h"
